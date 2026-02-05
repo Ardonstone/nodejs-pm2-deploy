0ac98dff21058a8e7ced275c0813661405bbbc93
Ardonstone <mashkina16@yandex.ru>
2026-02-05 20:21:32 +0300

    crash-test and deploy

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,26 @@
+require('dotenv').config({ path: './.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_SSH_KEY, DEPLOY_REF, DEPLOY_REPO, DEPLOY_PATH,
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'api-service',
+    script: './app.js',
+  }],
+
+  deploy: {
+    production: {
+      ssh_options: 'StrictHostKeyChecking=no',
+      key: DEPLOY_SSH_KEY,
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `scp -i ${DEPLOY_SSH_KEY} ./.env ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend/`,
+      'post-deploy': 'cd backend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+    },
+  },
+};
